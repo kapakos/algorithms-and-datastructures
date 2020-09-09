7b9a7518b90eb00823cecb242ed6d30f975b03c4
Petros Kapakos <kapakospetros@gmail.com>
2020-09-09 10:03:36 -0400

    add some comments and typos correction

--- test/invert_binary_tree.spec.js
+++ test/invert_binary_tree.spec.js
@@ -2,7 +2,7 @@ const InvertBinaryTree = require('../lib/invert_binary_tree');
 const TreeNode = require('../lib/tree_node');
 const LevelorderTraversal = require('../lib/level_order_traversal');
 
-describe.only('InvertBinaryTree', () => {
+describe('InvertBinaryTree', () => {
   const simpleTree = new TreeNode(1,
     new TreeNode(2),
     new TreeNode(3)); 
